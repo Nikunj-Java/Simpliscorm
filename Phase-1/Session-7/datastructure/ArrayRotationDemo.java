8b6f458b265b25255bfd069b4549604b6095a6db
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-05 17:03:43 +0530

    Inheritance,association,Datastructures(array)

---
+++ Phase-1/Session-7/datastructure/ArrayRotationDemo.java
@@ -0,0 +1,30 @@
+package com.simplilearn.datastructure;
+
+class ArrayRotationDemo {
+	
+	
+	public void rotate(int nums[],int k) {
+		
+		if(k> nums.length) {
+			k= k% nums.length; //10 divided by 7 will give reminder as 3
+			
+			int result[]= new int[nums.length]; //create new array with the size of given array
+			
+			for(int i=0; i<k; i++) {
+				result[i] =nums[nums.length-k +i]; //7-3=4 this will skip 4 elements (3rd index) and rest elements will rotated
+			}
+			
+			int j=0;
+			
+			for (int i=k; i<nums.length ;i++) {
+				result[i]=nums[j];
+				j++;
+			}
+			
+			System.arraycopy(result, 0, nums, 0, nums.length);
+		}
+		
+	}
+	
+
+}
