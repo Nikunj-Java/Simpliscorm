607adfee26fe8efe77382c17899ddf012218cbaa
unknown <ganitniduniyanikunj10@gmail.com>
2022-08-08 14:48:05 +0530

    Hibernate with annotations

--- Phase-2/Session-6/HibernateAnnotationJAVA/src/com/simplilearn/demo/Student.java
+++ Phase-2/Session-6/HibernateAnnotationJAVA/src/com/simplilearn/demo/Student.java
@@ -8,57 +8,50 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="student_table")
+@Table(name="student_data")
 public class Student {
 	
-	@Id //primary key
+	@Id//primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//auto generation
 	@Column(name="sid")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)// autoincrement
 	private int id;
 	
-	@Column(name="student_name")
+	@Column(name="sname")
 	private String name;
+	
 	private String email;
 	private String password;
-
+	
 	public Student() {
-		// TODO Auto-generated constructor stub
+		
 	}
-
-	public Student( String name, String email, String password) {
+	public Student(String name, String email, String password) {
+		super();
 		this.name = name;
 		this.email = email;
 		this.password = password;
 	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -66,4 +59,5 @@ public class Student {
 	
 	
 	
+
 }
