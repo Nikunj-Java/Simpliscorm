ef552ae0190145694a315dbd7b6fdb5e4844455b
unknown <ganitniduniyanikunj10@gmail.com>
2022-10-14 14:34:21 +0530

    Routing Mechanism

--- Phase-4/Session-6/form/src/app/app-routing.module.ts
+++ Phase-4/Session-6/form/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectiveComponent } from './directive/directive.component';
+import { PipdemoComponent } from './pipdemo/pipdemo.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {"path":"directive",component:DirectiveComponent},
+  {"path":"pipe",component:PipdemoComponent},
+  {"path":"register",component:RegisterComponent},
+  {"path":"user",component:UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
