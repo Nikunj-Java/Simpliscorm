e0fee9860a702a3b093924d31dd2bcdd4812ef72
unknown <ganitniduniyanikunj10@gmail.com>
2022-10-14 15:06:31 +0530

    Routing Mechanism,Http Service

--- Phase-4/Session-7/Routing/src/app/app-routing.module.ts
+++ Phase-4/Session-7/Routing/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   //path to redirect
   {"path":"home",component:HomeComponent},
   {"path":"about",component:AboutusComponent},
-  {"path":"contact",component:ContactusComponent}
+  {"path":"contact",component:ContactusComponent},
+  {"path":"user",component:UserComponent}
 ];
 
 @NgModule({
